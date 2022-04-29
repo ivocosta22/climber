50b2ee5688fcaef660cf6059abd475e19e1a984c
ivocosta22 <ivo.palhares.costa@gmail.com>
2022-04-29 13:10:32 +0100

    Implemented Google and Apple Sign in file dependencies

--- firebase.js
+++ firebase.js
@@ -4,11 +4,11 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyARNF5hpef55I2VrlhNKjwIitXFGPPNNQU",
-  authDomain: "climber-b8250.firebaseapp.com",
-  projectId: "climber-b8250",
-  storageBucket: "climber-b8250.appspot.com",
-  messagingSenderId: "1086214763413",
-  appId: "1:1086214763413:web:ccde2268d74059f31a3c9c",
-  measurementId: "G-V9CX2JQX63"
+  apiKey: "AIzaSyDSFjmtdZ0KFpUZSuu_97vGcX9ajfHD6aQ",
+  authDomain: "climber-3b27e.firebaseapp.com",
+  projectId: "climber-3b27e",
+  storageBucket: "climber-3b27e.appspot.com",
+  messagingSenderId: "806149464222",
+  appId: "1:806149464222:web:94711ec5f362239b035b8f",
+  measurementId: "G-WT2V921ZQV"
 };

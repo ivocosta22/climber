b9c4bdf662c772c4941e9774d3034f2fd2791b8a
ivocosta22 <ivo.palhares.costa@gmail.com>
2022-05-06 09:59:26 +0100

    Fixed stability bugs with launching app on android

--- navigation/screens/SettingsScreen.js
+++ navigation/screens/SettingsScreen.js
@@ -4,9 +4,9 @@ import { View, Text } from 'react-native';
 export default function SettingsScreen({navigation}) {
     return(
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style= {{ fontSize: 26, fontWeight: 'bold'}}>Settings Screen</Text>
+            <Text onPress={() => navigation.navigate('Home')} style= {{ fontSize: 26, fontWeight: 'bold'}}>
+                Settings Screen
+            </Text>
         </View>
     );
 }

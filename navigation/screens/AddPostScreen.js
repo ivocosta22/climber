d8e0f3289a55ee0a906e00e935931499be90c6e3
ivocosta22 <ivo.palhares.costa@gmail.com>
2022-06-09 15:07:46 +0100

    Added Post Screen UI, Taking Pictures, Choosing Photos UI

--- navigation/screens/AddPostScreen.js
+++ navigation/screens/AddPostScreen.js
@@ -1,13 +1,71 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
-import { InputField, InputWrapper } from '../../styles/AddPost'
+import { InputField, InputWrapper, AddImage } from '../../styles/AddPost'
 import ActionButton from 'react-native-action-button'
 import Icon from 'react-native-vector-icons/Ionicons'
+import * as ImagePicker from 'expo-image-picker'
+import { Camera } from 'expo-camera'
+import { Button } from 'react-native-paper'
 
 const AddPostScreen = () => {
+
+    const [hasCameraPermission, setHasCameraPermission] = React.useState(null)
+    const [hasGalleryPermission, setHasGalleryPermission] = React.useState(null)
+    const [camera, setCamera] = React.useState(null)
+    const [image, setImage] = React.useState(null)
+    const [type, setType] = React.useState(Camera.Constants.Type.back)
+
+    React.useEffect(() => {
+        (async () => {
+            const cameraPermission = await Camera.requestCameraPermissionsAsync()
+            const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync()
+            setHasCameraPermission(cameraPermission.status === 'granted')
+            setHasGalleryPermission(galleryStatus.status === 'granted')
+        })()
+    },[])
+
+    const takePicture = async () => {
+        if (hasCameraPermission === false) {
+            return <Text>Please give camera permissions to the aplication.</Text>
+        }
+        if (camera) {
+            const data = await camera.takePictureAsync(null)
+            setImage(data.uri)
+        }
+    }
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        })
+        console.log(result)
+        if (!result.cancelled) {
+            setImage(result.uri)
+        }
+    }
+
     return(
         <View style={styles.container}>
+
+            <View hidden style={{flex:1}}>
+                <View style={styles.cameraContainer}>
+                    <Camera ref={ref => setCamera(ref)}
+                    style={styles.fixedRatio}
+                    type={type}
+                    ratio={'1:1'}/>
+                </View>
+                <Button title="Flip Camera" onPress={() => {
+                    setType(type === Camera.Constants.Type.back ? Camera.Constants.Type.front:Camera.Constants.Type.back)
+                }}></Button>
+                <Button title="Take Picture" onPress={() => takePicture()}></Button>
+                {image && <Image source={{uri: image}} style={{flex:1}} />}
+            </View>
+
             <InputWrapper>
+            {/*image != null ? <AddImage source={{uri: image}} /> : null*/}
                 <InputField
                     placeholder = "What goal did you achieve?"
                     multiline
@@ -18,15 +76,13 @@ const AddPostScreen = () => {
                 <ActionButton.Item
                     buttonColor="#9b59b6"
                     title="Take Photo"
-                    //TODO
-                    onPress={() => {}}>
+                    onPress={takePicture}>
                     <Icon name="camera-outline" style={styles.actionButtonIcon} />
                 </ActionButton.Item>
                 <ActionButton.Item
                     buttonColor="#e84d3c"
                     title="Choose Photo"
-                    //TODO
-                    onPress={() => {}}>
+                    onPress={pickImage}>
                     <Icon name="md-images-outline" style={styles.actionButtonIcon} />
                 </ActionButton.Item>
             </ActionButton>
@@ -47,4 +103,12 @@ const styles = StyleSheet.create({
         height: 22,
         color: 'white',
     },
+    cameraContainer: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    fixedRatio: {
+        flex: 1,
+        aspectRatio: 1
+    }
 })
